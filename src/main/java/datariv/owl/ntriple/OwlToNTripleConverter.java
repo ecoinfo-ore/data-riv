4c546c10ea14653037ccfe5cf743119a985693f4
ecoinfo-ore <37690220+ecoinfo-ore@users.noreply.github.com>
2020-04-19 04:45:23 +0200

    Update OwlToNTripleConverter.java

--- src/main/java/datariv/owl/ntriple/OwlToNTripleConverter.java
+++ src/main/java/datariv/owl/ntriple/OwlToNTripleConverter.java
@@ -21,17 +21,24 @@ public class OwlToNTripleConverter {
   
     public static void convert( String owlPath, String outPathFile ) throws FileNotFoundException {
         
+      LOGGER.info("Reading Ontolgy File : " + owlPath )          ;
+      
       OntModelSpec spec = OntModelSpec.OWL_DL_MEM_RDFS_INF       ;
  
       Model model       = ModelFactory.createOntologyModel(spec) ;
-
+      
       model.read( owlPath ) ;
       
-      File file            = new File( outPathFile )      ;
-      FileOutputStream fop = new FileOutputStream( file ) ;
+      File file            = new File ( outPathFile )            ;
+      FileOutputStream fop = new FileOutputStream( file  )       ;
 
+      LOGGER.info( "Writing Ontology in [ TTL ] Format at : "    + 
+                   outPathFile                              )    ;
+      
       model.write( fop , "NT" ) ;
     
+      LOGGER.info(" DONE !  " ) ;
+      
     }
- 
 }
+
